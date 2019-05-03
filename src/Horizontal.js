a258cddedb5453686afa34a3ec30d376aad59086
billouboq <aurelien.boquet.web@gmail.com>
2019-05-03 15:27:02 +0200

    rework component to take style props

--- src/Horizontal.js
+++ src/Horizontal.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+	View,
 	PanResponder,
 	Animated,
-	Dimensions,
 	StyleSheet,
 	Platform
 } from 'react-native';
-const { width, height } = Dimensions.get('window');
 
 const PESPECTIVE = Platform.OS === 'ios' ? 2.38 : 1.7;
 const TR_POSITION = Platform.OS === 'ios' ? 2 : 1.5;
@@ -18,19 +17,14 @@ const getChildrenArray = (children) => {
 };
 
 export default class CubeNavigationHorizontal extends React.PureComponent {
-	constructor(props) {
-		super(props);
-
-		const children = getChildrenArray(props.children);
-
-		this.pages = children.map((_, index) => width * -index);
-
-		this.state = {
-			currentPageIndex: 0
-		};
-	}
+	state = {
+		pagesWidth: [],
+		containerWidth: null,
+		currentPageIndex: 0
+	};
 
 	componentWillMount() {
+		const { pagesWidth } = this.state;
 		this._animatedValue = new Animated.ValueXY();
 		this._animatedValue.setValue({ x: 0, y: 0 });
 		this._value = { x: 0, y: 0 };
@@ -52,40 +46,24 @@ export default class CubeNavigationHorizontal extends React.PureComponent {
 				Animated.event([null, { dx: this._animatedValue.x }])(e, gestureState);
 
 				// Avoid last movement
-				this.lockLast = this.pages[this.pages.length - 1];
+				this.lockLast = pagesWidth[pagesWidth.length - 1];
 
-				if (this._value.x > this.pages[0] || this._value.x < this.lockLast) {
+				if (this._value.x > pagesWidth[0] || this._value.x < this.lockLast) {
 					this._animatedValue.setValue({ x: 0, y: 0 });
 				}
 			},
 			onPanResponderRelease: (e, gestureState) => {
-				const { currentPageIndex } = this.state;
 				const mod = gestureState.dx > 0 ? 100 : -100;
-				const { ans: goTo, pageIndex } = this._closest(this._value.x + mod);
+				const { pageWidth, pageIndex } = this._closest(this._value.x + mod);
 
-				if (this.lockLast > goTo) return; //remove in the future
+				if (this.lockLast > pageWidth) return; //remove in the future
 
 				this._animatedValue.flattenOffset({
 					x: this._value.x,
 					y: this._value.y
 				});
 
-				if (currentPageIndex !== pageIndex) {
-					this.props.onBeforePageChange(pageIndex);
-				}
-
-				Animated.spring(this._animatedValue, {
-					toValue: { x: goTo, y: 0 },
-					friction: 3,
-					tension: 0.6,
-					useNativeDriver: true
-				}).start(() => {
-					if (currentPageIndex !== pageIndex) {
-						this.setState({ currentPageIndex: pageIndex }, () => {
-							this.props.onPageChange(pageIndex);
-						});
-					}
-				});
+				this._changePage(pageIndex);
 			}
 		});
 	}
@@ -93,46 +71,67 @@ export default class CubeNavigationHorizontal extends React.PureComponent {
 	/*
     @page: index
   */
-	scrollTo = (index, animated = true) => {
-		const pageWidth = this.pages[index];
-
-		if (pageWidth == null && typeof pageWidth !== 'number') {
-			return;
-		}
-
-		if (animated) {
-			Animated.spring(this._animatedValue, {
-				toValue: { x: pageWidth, y: 0 },
-				friction: 4,
-				tension: 0.8,
-				useNativeDriver: true
-			}).start();
-		} else {
-			this._animatedValue.setValue({ x: pageWidth, y: 0 });
-		}
+	scrollTo = (pageIndex) => {
+		this._changePage(pageIndex);
 	};
 
 	/*
   Private methods
   */
+	_storeContainerSize = ({ nativeEvent: { layout } }) => {
+		const children = getChildrenArray(this.props.children);
+		const containerWidth = layout.width;
+
+		this.setState({
+			pagesWidth: children.map((_, index) => containerWidth * -index),
+			containerWidth: containerWidth
+		});
+	};
+
+	_changePage = (pageIndex) => {
+		const { currentPageIndex, pagesWidth } = this.state;
+		const pageWidth = pagesWidth[pageIndex];
+
+		if (pageWidth == null && typeof pageWidth !== 'number') {
+			return;
+		}
+
+		if (currentPageIndex !== pageIndex) {
+			this.setState({ currentPageIndex: pageIndex }, () => {
+				this.props.onBeforePageChange(pageIndex);
+			});
+		}
+
+		Animated.spring(this._animatedValue, {
+			toValue: { x: pageWidth, y: 0 },
+			friction: 3,
+			tension: 0.6,
+			useNativeDriver: true
+		}).start(() => {
+			if (currentPageIndex !== pageIndex) {
+				this.props.onPageChange(pageIndex);
+			}
+		});
+	};
 
 	_getTransformsFor = (i) => {
-		let scrollX = this._animatedValue.x;
-		let pageX = -width * i;
+		const { containerWidth: width } = this.state;
+		const scrollX = this._animatedValue.x;
+		const pageX = -width * i;
 
-		let translateX = scrollX.interpolate({
+		const translateX = scrollX.interpolate({
 			inputRange: [pageX - width, pageX, pageX + width],
 			outputRange: [(-width - 1) / TR_POSITION, 0, (width + 1) / TR_POSITION],
 			extrapolate: 'clamp'
 		});
 
-		let rotateY = scrollX.interpolate({
+		const rotateY = scrollX.interpolate({
 			inputRange: [pageX - width, pageX, pageX + width],
 			outputRange: ['-60deg', '0deg', '60deg'],
 			extrapolate: 'clamp'
 		});
 
-		let translateXAfterRotate = scrollX.interpolate({
+		const translateXAfterRotate = scrollX.interpolate({
 			inputRange: [pageX - width, pageX, pageX + width],
 			inputRange: [
 				pageX - width,
@@ -151,7 +150,7 @@ export default class CubeNavigationHorizontal extends React.PureComponent {
 			extrapolate: 'clamp'
 		});
 
-		let opacity = scrollX.interpolate({
+		const opacity = scrollX.interpolate({
 			inputRange: [
 				pageX - width,
 				pageX - width + 10,
@@ -175,16 +174,6 @@ export default class CubeNavigationHorizontal extends React.PureComponent {
 	};
 
 	_renderChild = (child, i) => {
-		let expandStyle = this.props.expandView
-			? { paddingTop: 100, paddingBottom: 100, height: height + 200 }
-			: { width, height };
-		let style = [child.props.style, expandStyle];
-		let props = {
-			i,
-			style
-		};
-		let element = React.cloneElement(child, props);
-
 		return (
 			<Animated.View
 				style={[
@@ -192,69 +181,60 @@ export default class CubeNavigationHorizontal extends React.PureComponent {
 					{ backgroundColor: 'transparent' },
 					this._getTransformsFor(i)
 				]}
-				key={`child- ${i}`}
+				key={`child-${i}`}
 			>
-				{element}
+				{child}
 			</Animated.View>
 		);
 	};
 
 	_closest = (num) => {
-		let array = this.pages;
-
-		let i = 0;
-		let pageIndex;
 		let minDiff = 1000;
-		let ans;
 
-		for (i in array) {
-			let m = Math.abs(num - array[i]);
+		return this.state.pagesWidth.reduce((memo, pageWidth, i) => {
+			const m = Math.abs(num - pageWidth);
+
 			if (m < minDiff) {
 				minDiff = m;
-				ans = array[i];
-				pageIndex = i;
+				memo.pageWidth = pageWidth;
+				memo.pageIndex = i;
 			}
-		}
 
-		return { ans, pageIndex };
+			return memo;
+		}, {});
 	};
 
 	render() {
-		const { expandView, children } = this.props;
-		const childrenArray = getChildrenArray(children);
-		const expandStyle = expandView
-			? { top: -100, left: 0, width, height: height + 200 }
-			: { width, height };
+		const { children, style } = this.props;
+		const { containerWidth } = this.state;
+
+		if (!containerWidth) {
+			return (
+				<View
+					style={StyleSheet.absoluteFill}
+					onLayout={this._storeContainerSize}
+				/>
+			);
+		}
 
 		return (
-			<Animated.View
-				style={[{ position: 'absolute' }]}
-				ref={(view) => {
-					this._scrollView = view;
-				}}
+			<View
+				style={[{ backgroundColor: 'black' }, style]}
 				{...this._panResponder.panHandlers}
 			>
-				<Animated.View
-					style={[
-						{ backgroundColor: '#000', position: 'absolute', width, height },
-						expandStyle
-					]}
-				>
-					{childrenArray.map(this._renderChild)}
-				</Animated.View>
-			</Animated.View>
+				{getChildrenArray(children).map(this._renderChild)}
+			</View>
 		);
 	}
 }
 
 CubeNavigationHorizontal.propTypes = {
+	style: PropTypes.any,
 	onPageChange: PropTypes.func,
-	onBeforePageChange: PropTypes.func,
-	expandView: PropTypes.bool
+	onBeforePageChange: PropTypes.func
 };
 
 CubeNavigationHorizontal.defaultProps = {
-	expandView: false,
 	onPageChange: () => {},
 	onBeforePageChange: () => {}
 };
